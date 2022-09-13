c8916230c135fcdb1a50ade7f0e10b355f1e115c
Agureeva Anna <agureeva@ro.ru>
2022-09-14 00:00:38 +0300

    сделала форматирование

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        long [] sale = {3, 34, 66, 76, 89};
+        long[] sale = {3, 34, 66, 76, 89};
         SalesManager salesManager = new SalesManager(sale);
         long max = salesManager.max();
         System.out.println(max);
