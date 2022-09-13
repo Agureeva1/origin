eaa6c8dc665a8422d2b9bfc4aa78c1cb23a79384
Agureeva Anna <agureeva@ro.ru>
2022-09-13 23:38:31 +0300

    Добавила метод avarageSales

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-      int[] sale = {3, 34, 66, 76, 89};
-        SalesManager salesManager = new SalesManager (sale);
+        int[] sale = {3, 34, 66, 76, 89};
+        SalesManager salesManager = new SalesManager(sale);
         int max = salesManager.max();
         System.out.println(max);
+        System.out.println(salesManager.averageSales());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -5,6 +5,7 @@ public class SalesManager {
         this.sales = sales;
     }
 
+
     public int max() {
         int max = -1;
         for (int sale : sales) {
@@ -14,4 +15,23 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int averageSales() {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return (sum - min() - max()) / (sales.length - 2);
+    }
+
 }
